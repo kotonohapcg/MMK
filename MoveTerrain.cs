2dcc49c270fdcd064390c117722c1c1088140584
kotonohapcg <tukudatktk06@gmail.com>
2019-11-11 15:15:02 +0900

    Terrainを動かすスクリプト①

---
+++ MoveTerrain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveTerrain : MonoBehaviour {
+    public float speed;
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate (0, 0, speed);
+    }
+}
