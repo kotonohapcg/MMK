83d035ee5c3fe975add9476c295c9df2901e8eec
kotonohapcg <tukudatktk06@gmail.com>
2019-11-13 11:51:10 +0900

    プロトタイプ開発時の重要プレハブ類
    プロトタイプ開発時の重要プレハブ類をバックアップする

---
+++ Assets/SCRIPTS/TitleScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("DifficultScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
